ef58f56fcb175e014b854cbc2ab533663636e804
Ilia Gladkikh <gntinbv@yandex.ru>
2020-11-01 21:19:57 +0300

    Подключен JS, нужно закончить со стилями popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,30 @@
-const popup = document.querySelector('.popup');
-const popupCloseButton = document.querySelector('.popup__close');
-const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector(".popup");
+const popupCloseButton = document.querySelector(".popup__close");
+const editButton = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const popupForm = document.querySelector(".popup__form");
+const nameFieldPopup = document.querySelector('.popup__input_type_name');
+const descriptionFieldPopup = document.querySelector('.popup__input_type_description');
 
 function showPopup() {
-popup.classList.add('popup_opened');
+  popup.classList.add("popup_opened");
+  nameFieldPopup.value = profileName.textContent;
+  descriptionFieldPopup.value = profileDescription.textContent;
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closePopup(event) {
+  popup.classList.remove("popup_opened");
 }
 
-editButton.addEventListener('click', showPopup);
-popupCloseButton.addEventListener('click', closePopup);
+editButton.addEventListener("click", showPopup);
+popupCloseButton.addEventListener("click", closePopup);
+
+function submitForm(event) {
+  event.preventDefault(); /* этот код предотвращает выполнение действий браузера "по-умолчанию" помимо тех, что указали мы */
+  profileName.textContent = nameFieldPopup.value;
+  profileDescription.textContent = descriptionFieldPopup.value;
+  closePopup()
+}
+
+popupForm.addEventListener("submit", submitForm);
