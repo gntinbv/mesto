58bf3c9b8bd584046b24477a3c3e7e7753b7036f
Ilia Gladkikh <gntinbv@yandex.ru>
2020-11-17 20:54:20 +0300

    Добавлена загрузка карточек из массива темплейтами

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,46 @@ const nameFieldPopup = document.querySelector('.popup__input_type_name');
 const titleFieldPopup = document.querySelector('.popup__input_type_title');
 const urlFieldPopup = document.querySelector('.popup__input_type_url');
 const descriptionFieldPopup = document.querySelector('.popup__input_type_description');
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  { 
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+const elementsContainer = document.querySelector('.elements');
+
+function addCard(cardName, cardUrl) {
+  const cardElement = document.querySelector('#card-template').content.cloneNode(true);
+  cardElement.querySelector('.element__image').src = cardUrl;
+  cardElement.querySelector('.element__title').textContent = cardName;
+
+  elementsContainer.append(cardElement);
+}
+
+initialCards.forEach((item) => addCard(item.name, item.link));
+
+
 
 function showPopupTypeEdit() {
   popupTypeEdit.classList.add('popup_opened');
